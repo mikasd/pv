a5ab9d87567f75585bf1a654f407aa080bb47195
mingley <54518@icf.com>
2021-11-02 07:23:36 -0700

    adds rewriting number to update in real time in console as bytes are parsed

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
 const BUFFER_SIZE: usize = 16 * 1024;
 
-fn main() {
+fn main() -> Result<()> {
     // bool that checks if env var exists
     let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
+    let mut buffer = [0; BUFFER_SIZE];
 
     loop {
-        let mut buffer = [0; BUFFER_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
         };
         total_bytes += num_read;
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if !silent {
+            eprint!("\r{}", total_bytes);
+        }
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+        }
     }
-    if !silent {
-        eprintln!("total bytes: {}", total_bytes);
-    }
+
+    Ok(())
 }
